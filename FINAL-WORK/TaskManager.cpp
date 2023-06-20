b8516f7758cfe73a2940899671c84d180b7bb1f0
VidelaGuliotti <tobiasvidelaug14@gmail.com>
2023-06-20 13:26:44 -0300

    Updating TM: now we can add a sample task to work

--- FINAL-WORK/TaskManager.cpp
+++ FINAL-WORK/TaskManager.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define mil 5
+#define mil 1000
 
-struct Tarea {
-    char *title;
-    char *description;
+//structs
+struct Task {
+    char title[100];
+    char description[500];
     int difficulty; //Easy-Medium-Hard||1-2-3
-    int state; //Pending-Current-Finished-Canceled||1-2-3-4
+    int state; //Pending-In progress-Finished-Canceled||1-2-3-4
     /*      DATES
      *  char creation[30];
      *  char expiration[30];
@@ -15,10 +16,17 @@ struct Tarea {
     */
 };
 
+//global variables
+struct Task Tasks[mil]; //all tasks
+int lastAdded = 0; //last task added
+
 //Menus
+void printTitle(){
+    printf("\t\t\t TASK MANAGER v1 \n");
+}
 void printMainMenu(){
     system("cls");
-    printf("\t\t\t TASK MANAGER v1 \n");
+    printTitle();
     printf("\n Welcome User! What do you want to do? \n");
     printf("\n");
     printf("\t[1] See my tasks.\n");
@@ -29,19 +37,19 @@ void printMainMenu(){
 }
 void printSeeMenu(){
     system("cls");
-    printf("\t\t\tTASK MANAGER v1\n");
+    printTitle();
     printf("\n Sure! Which tasks do you want to see?\n");
     printf("\n");
     printf("\t[1] All my tasks.\n");
     printf("\t[2] Pending tasks.\n");
-    printf("\t[3] Current tasks.\n");
+    printf("\t[3] Tasks in progress.\n");
     printf("\t[4] Finished tasks.\n");
-    printf("\t[0] Main Menu.\n");
+    printf("\t[0] Go back.\n");
     printf("\n");
 }
 void printSearchMenu(){
     system("cls");
-    printf("\t\t\tTASK MANAGER v1\n");
+    printTitle();
     printf("\n Fine! Type anything to look for... Or 0 (zero) to go back.\n");
     printf("\n");
     //Read and print the matching tasks :D and so on
@@ -49,7 +57,7 @@ void printSearchMenu(){
 }
 void printAddMenu(){
     system("cls");
-    printf("\t\t\tTASK MANAGER v1\n");
+    printTitle();
     printf("\n Good! There you go:\n");
     printf("\n");
     /*          Create a New Task
@@ -62,83 +70,66 @@ void printAddMenu(){
     printf("\n");
 }
 
-int main () {
-    struct Tarea Tareas[mil];
-    for(int i = 0; i < strlen(Tareas); i++){
-        printf(" Task %d\n",i);
-        printf("\n\t Title:");
-        gets(Tareas[i].title);
-        printf("\n\t Description:");
-        gets(Tareas[i].description);
-        printf("\n\t Difficulty:");
-        scanf("%d",&Tareas[i].difficulty);
-        printf("\n\t State:");
-        scanf("%d",&Tareas[i].state);
+//Functions
+void selectMenus(int sel){
+    switch (sel) {
+        case 1: //See
+            int sel;
+            do{
+                printSeeMenu();
+                scanf("%d",&sel);
+                //SubMenu
+            } while (sel != 0);
+            break;
+        case 2: //Search
+            do{
+                printSearchMenu();
+                scanf("%d",&sel);
+                //Search tasks
+            } while (sel != 0);
+            break;
+        case 3: //Add
+            do {
+                printAddMenu();
+                //addTask()
+                scanf("%d",&sel); //temporary
+            } while (sel != 0);
+            break;
     }
+}
+void addTask(){
+    fflush(stdin);
+    printf("\n\t Title:");
+    gets(Tasks[lastAdded].title);
+    printf("\n\t Description:");
+    gets(Tasks[lastAdded].description);
+    printf("\n\t Difficulty:");
+    scanf("%d",&Tasks[lastAdded].difficulty);
+    printf("\n\t State:");
+    scanf("%d",&Tasks[lastAdded].state);
 
-    for(int i = 0; i < strlen(Tareas); i++){
-        printf("\n\t Tarea N°%d\n",i);
-        printf("\t\t Title: %s",Tareas[i].title);
-        printf("\t\t Description: %s",Tareas[i].description);
-        switch (Tareas[i].difficulty) {
-            case 1:
-                printf("\t\t Difficulty: Easy");
-                break;
-            case 2:
-                printf("\t\t Difficulty: Medium");
-                break;
-            case 3:
-                printf("\t\t Difficulty: Hard");
-                break;
-        }
-        switch (Tareas[i].state) {
-            case 1:
-                printf("\t\t State: Pending");
-                break;
-            case 2:
-                printf("\t\t State: Current");
-                break;
-            case 3:
-                printf("\t\t State: Finished");
-                break;
-            case 4:
-                printf("\t\t State: Canceled");
-                break;
-        }
-    }
-/*
+    lastAdded++;
+}
+
+int main () {
+
     //variables
     int sel;
+    addTask();
 
+    //App
     do{
         printMainMenu();
         scanf("%d",&sel);
 
         //Enter to subMenus
-        switch (sel) {
-            case 1: //See
-                int sel;
-                do{
-                    printSeeMenu();
-                    scanf("%d",&sel);
-                } while (sel != 0);
-                break;
-            case 2: //Search
-                do{
-                    printSearchMenu();
-                    scanf("%d",&sel);
-                } while (sel != 0);
-                break;
-            case 3: //Add
-                do {
-                    printAddMenu();
-                    scanf("%d",&sel);
-                } while (sel != 0);
-                break;
-        }
+        selectMenus(sel);
+
+
+
     } while (sel != 0);
 
     printf("\nExiting...\n\n");
-*/
+
     return 0;
 }
