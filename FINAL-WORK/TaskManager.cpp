3636ac44c09a7ce084c583dd48749ecd7fa768dd
VidelaGuliotti <tobiasvidelaug14@gmail.com>
2023-06-20 18:11:36 -0300

    Updating TM: succesfully added most of the features for editing and seeing details...

--- FINAL-WORK/TaskManager.cpp
+++ FINAL-WORK/TaskManager.cpp
@@ -29,14 +29,17 @@ struct Task Tasks[mil]; //all tasks
 int lastAdded = 0; //(last position of array used) + 1
 
 //Menus
+
 void printTitle(){
     printf("\t\t\t TASK MANAGER v1 \n");
 }
+
 void printBackOrDetails(){
     printf("\n What do you want to do next?");
     printf("\n\t [0] Go back.");
-    printf("\n\t [...] Select the number of any listed task to go to the Details Menu.\n");
+    printf("\n\t [...] Select the number of any listed task to go to its Details Menu.\n");
 }
+
 void printMainMenu(){
     system("cls");
     printTitle();
@@ -48,6 +51,7 @@ void printMainMenu(){
     printf("\t[0] Exit.\n");
     printf("\n");
 }
+
 void printSeeMenu(){
     system("cls");
     printTitle();
@@ -60,11 +64,13 @@ void printSeeMenu(){
     printf("\t[0] Go back.\n");
     printf("\n");
 }
+
 void printSearchMenu(){
     system("cls");
     printTitle();
     printf("\n Fine! Type a word or phrase to look for: ");
 }
+
 void printAddMenu(){
     system("cls");
     printTitle();
@@ -79,42 +85,51 @@ void printAddMenu(){
     printf("\n(Type 0 to go back.)"); //temporary
     printf("\n");
 }
+
 void printDetailsMenu(int selectedTask){
     //selectedTask-1 is the actual position of the selected task
     system("cls");
     printTitle();
     printf("\n Details Menu of \"%s\":\n\n",Tasks[selectedTask-1].title);
     printf("\t %s \n\n",Tasks[selectedTask-1].title);
-    printf("\t %s \n\n",Tasks[selectedTask-1].description);
+    printf("\t %s \n\n\n",Tasks[selectedTask-1].description);
     switch(Tasks[selectedTask-1].state){
         case 1:
-            printf("\t State:               Pending \n\n");
+            printf("\t State:               Pending \n");
             break;
         case 2:
-            printf("\t State:               In Progress \n\n");
+            printf("\t State:               In Progress \n");
             break;
         case 3:
-            printf("\t State:               Finished \n\n");
+            printf("\t State:               Finished \u263A \n");
             break;
     }
     switch(Tasks[selectedTask-1].difficulty){
         case 1:
-            printf("\t Difficulty:          Easy \n\n");
+            printf("\t Difficulty:          Easy \n");
             break;
         case 2:
-            printf("\t Difficulty:          Medium \n\n");
+            printf("\t Difficulty:          Medium \n");
             break;
         case 3:
-            printf("\t Difficulty:          Hard \n\n");
+            printf("\t Difficulty:          Hard \n");
             break;
     }
     /*
-    printf("\t Creation Date:       %s \n\n",Tasks[selectedTask-1].creation);
-    printf("\t Expiration Date:     %s \n\n",Tasks[selectedTask-1].expiration);
-    printf("\t Last Edit Date:      %s \n\n",Tasks[selectedTask-1].lastEdit);
+    printf("\t Creation Date:       %s \n",Tasks[selectedTask-1].creation);
+    printf("\t Expiration Date:     %s \n",Tasks[selectedTask-1].expiration);
+    printf("\t Last Edit Date:      %s \n",Tasks[selectedTask-1].lastEdit);
     */
     printf("\n");
     printf("\n Type 0 to go back, or 1 to edit.\n");
+}
+
+void printEditMenu(int selectedTask){
+    //selectedTask-1 is the actual position of the selected task
+    system("cls");
+    printTitle();
+    printf("\n You are about to edit \"%s\".\n",Tasks[selectedTask-1].title);
+    printf("\t -Write a 0 (zero) to not edit an attribute.\n");
 }
 
 void printAllTasks(){
@@ -128,6 +143,7 @@ void printAllTasks(){
     printBackOrDetails();
     printf("\n");
 }
+
 void printPendingTasks(){
     system("cls");
     printTitle();
@@ -141,6 +157,7 @@ void printPendingTasks(){
     printBackOrDetails();
     printf("\n");
 }
+
 void printInProgressTasks(){
     system("cls");
     printTitle();
@@ -154,6 +171,7 @@ void printInProgressTasks(){
     printBackOrDetails();
     printf("\n");
 }
+
 void printFinishedTasks(){
     system("cls");
     printTitle();
@@ -167,6 +185,7 @@ void printFinishedTasks(){
     printBackOrDetails();
     printf("\n");
 }
+
 void printMatches(char *key){
     printf("\nThese are the matches:\n");
     for(int i = 0; i < lastAdded; i++){
@@ -181,6 +200,58 @@ void printMatches(char *key){
 
 
 //Functions
+
+void editTask(int selectedTask){
+    char newTitle[100];
+    char newDescription[500];
+    int newDifficulty;
+    int newState;
+    //New Expiration Date
+
+    fflush(stdin);
+    printf("\n\t New Title (Max. 100 characters) : ");
+    gets(newTitle);
+    if (strcmp(newTitle,"0") != 0) {
+        strcpy(Tasks[selectedTask-1].title, newTitle);
+    }
+
+    fflush(stdin);
+    printf("\n\t New Description (Max. 500 characters) : ");
+    gets(newDescription);
+    if (strcmp(newDescription,"0") != 0) {
+        strcpy(Tasks[selectedTask-1].description, newDescription);
+    }
+
+    fflush(stdin);
+    printf("\n\t New Difficulty (1 - 2 - 3) : ");
+    do {
+        scanf("%d",&newDifficulty);
+        if (newDifficulty != 0) {
+            if (newDifficulty < 3 && newDifficulty > 0) {
+                Tasks[selectedTask-1].difficulty = newDifficulty;
+            }
+        }
+    } while (newDifficulty > 3 || newDifficulty < 0);
+
+    fflush(stdin);
+    printf("\n\t New State ([1]Pending - [2]In Progress - [3]Finished - [4]Canceled) : ");
+    do {
+        scanf("%d",&newState);
+        if (newState != 0) {
+            if (newState < 5 && newState > 0) {
+                Tasks[selectedTask-1].state = newState;
+            }
+
+        }
+    } while (newState > 5 || newState < 0);
+
+
+    printf("\n");
+    printf("\nChanges saved!");
+    printf("\n");
+    printf("\t Type 0 (zero) to continue...");
+}
+
 void listTasks(int sel){
     switch (sel) {
         case 1: //All Tasks
@@ -188,28 +259,77 @@ void listTasks(int sel){
             do{
                 printAllTasks();
                 scanf("%d",&sel);
+                if (sel != 0){
+                    do {
+                        int selectedTask = sel;
+                        printDetailsMenu(sel);
+                        scanf("%d",&sel);
+                        if (sel == 1) {
+                            printEditMenu(selectedTask);
+                            editTask(selectedTask);
+                            scanf("%d",&sel);
+                        }
+                    } while (sel != 0);
+                }
             } while (sel != 0);
             break;
         case 2: //Pending Tasks
             do{
                 printPendingTasks();
                 scanf("%d",&sel);
+                if (sel != 0){
+                    do {
+                        int selectedTask = sel;
+                        printDetailsMenu(sel);
+                        scanf("%d",&sel);
+                        if (sel == 1) {
+                            printEditMenu(selectedTask);
+                            editTask(selectedTask);
+                            scanf("%d",&sel);
+                        }
+                    } while (sel != 0);
+                }
             } while (sel != 0);
             break;
         case 3: //Tasks In Progress
             do{
                 printInProgressTasks();
                 scanf("%d",&sel);
+                if (sel != 0){
+                    do {
+                        int selectedTask = sel;
+                        printDetailsMenu(sel);
+                        scanf("%d",&sel);
+                        if (sel == 1) {
+                            printEditMenu(selectedTask);
+                            editTask(selectedTask);
+                            scanf("%d",&sel);
+                        }
+                    } while (sel != 0);
+                }
             } while (sel != 0);
             break;
         case 4: //Finished Tasks
             do{
                 printFinishedTasks();
                 scanf("%d",&sel);
+                if (sel != 0){
+                    do {
+                        int selectedTask = sel;
+                        printDetailsMenu(sel);
+                        scanf("%d",&sel);
+                        if (sel == 1) {
+                            printEditMenu(selectedTask);
+                            editTask(selectedTask);
+                            scanf("%d",&sel);
+                        }
+                    } while (sel != 0);
+                }
             } while (sel != 0);
             break;
     }
 }
+
 void searchTask(){
     int found = 0;
     char key[500];
@@ -236,6 +356,7 @@ void searchTask(){
         printf("\n Type 0 (zero) to go back.\n");
     }
 }
+
 void selectMenus(int sel){
     switch (sel) {
         case 1: //See
@@ -251,6 +372,7 @@ void selectMenus(int sel){
                 printSearchMenu();
                 searchTask();
                 scanf("%d",&sel);
+                //if sel!= 0 => editmenu
             } while (sel != 0);
             break;
         case 3: //Add
@@ -262,6 +384,7 @@ void selectMenus(int sel){
             break;
     }
 }
+
 void addTask(){
     fflush(stdin);
     printf("\n\t Title:");
@@ -280,6 +403,7 @@ int main () {
 
     //variables
     int sel;
+    addTask();
     addTask();
 
     //Execute App
